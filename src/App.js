99b66f2eaef8d755d66b861110859bc54a254b11
Shahria Jaman Khan <41187226+shahria7k@users.noreply.github.com>
2021-03-03 22:14:27 +0600

    css update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<div className="players">
 				{Data.map((post) => {
 					return (
-						<div>
+						<div className="card">
 							<h4>{post.name}</h4>
 							<img src={post.photo} alt="" />
 							<h4>{post.value}</h4>
